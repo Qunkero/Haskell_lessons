da69ec34d38374c4740fb111a691521c6db80c4a
Qunkero <dron.andrew2011@yandex.ua>
2018-05-20 20:11:53 +0300

    recursion with accumulator

--- recursion1_5/fibonacci.hs
+++ recursion1_5/fibonacci.hs
@@ -15,6 +15,23 @@ fibonacci2 n
   | n > 0 = fibonacci2 (n - 1) + fibonacci2 (n - 2)
   | otherwise = fibonacci2 (n + 2) - fibonacci2 (n + 1)
 
+fibonacci3 :: Integer -> Integer
+fibonacci3 n
+  | n == (-1) = 1
+  | n == 0 = 0
+  | n == 1 = 1
+  | n > 0 = helperPositive 1 1 n 2
+  | otherwise = helperNegative (-1) 1 n (-2)
+
+helperPositive :: Integer -> Integer -> Integer -> Integer -> Integer
+helperPositive acc2 acc1 n step
+  | n == step = acc2
+  | otherwise = helperPositive (acc2 + acc1) acc2 n (step + 1)
+
+helperNegative :: Integer -> Integer -> Integer -> Integer -> Integer
+helperNegative acc2 acc1 n step
+  | n == step = acc2
+  | otherwise = helperNegative (acc1 - acc2) acc2 n (step - 1)
 
 bmiTell :: Double -> String
 bmiTell bmi
