301a3e6f25129bb620dedae750f210b045fef83b
Qunkero <dron.andrew2011@yandex.ua>
2019-09-14 22:58:35 +0300

    max3

--- 3_2_hif_list/delUpper.hs
+++ 3_2_hif_list/delUpper.hs
@@ -4,3 +4,9 @@ import Data.Char
 
 delAllUper :: String -> String
 delAllUper = unwords . filter (not . all isUpper) . words
+
+max3 :: Ord a => [a] -> [a] -> [a] -> [a]
+max3 _ _ [] = []
+max3 _ [] _ = []
+max3 [] _ _ = []
+max3 (a:as) (b: bs) (c:cs) = max b (max c a) : max3 as bs cs
